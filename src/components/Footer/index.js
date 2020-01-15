5309089e935966f22a97d5696f2713b4ef1a6b3a
rthefj <rthefj@yonyou.com>
2020-01-15 16:29:25 +0800

    hh

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.less'
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+               版权所有：紫漠的管理系统（推荐使用谷歌浏览器，可以获得更佳操作页面体验） 技术支持：紫漠
+            </div>
+        );
+    }
+}
